47686cea25348af9824ee7057ef49333333733e8
arobbins <arobbins@simpleblend.net>
2014-07-01 19:31:43 -0500

    Fixed Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,20 @@ var gulp = require('gulp'),
 	livereload = require('gulp-livereload'),
 	jshint = require('gulp-jshint'),
 	rename = require('gulp-rename');
+	uglify = require('gulp-uglify');
 
-
-var target = {
-    sass_src: 'css/scss/*.scss',
-    css_dest: 'css',
-    css_libs: [
-    	'components/bootstrap-css-only/css/bootstrap.min.css'
-    ]
+var paths = {
+	scripts: [
+		'js/vendor/angular.min.js',
+		'js/vendor/angular-ui-router.min.js',
+		'js/vendor/angular-scroll.min.js',
+		'js/vendor/ui-bootstrap-tpls-0.11.0.min.js',
+		'js/vendor/ui-utils.min.js',
+		'js/app/app.js',
+		'js/app/controllers/*.js',
+		'js/app/services/*.js',
+		'js/app/directives/*.js'
+	]
 };
 
 function startExpress() {
@@ -21,25 +27,28 @@ function startExpress() {
 		app = express();
 
 	app.use(express.static(__dirname));
-	app.listen(8000);
+	app.listen(9012);
 }
 
-gulp.task('sass', function() {
+gulp.task('styles', function() {
     gulp.src('css/scss/main.scss')
-        .pipe(sass({includePaths: ['scss']}))
-        .pipe(minifyCSS())
-        .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('css'));
+		.pipe(sass({includePaths: ['scss']}))
+		.pipe(minifyCSS())
+		.pipe(rename('styles.min.css'))
+		.pipe(gulp.dest('css'));
 });
 
-gulp.task('concat', function() {
-    gulp.src(['components/bootstrap-css-only/css/bootstrap.min.css', 'css/styles.min.css'])
-        .pipe(concat('styles.min.css'))
-        .pipe(gulp.dest('css'));
+// Minify and concat all js scripts
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['sass', 'concat'], function() {
-    gulp.watch("css/scss/*.scss", ['sass']);
+gulp.task('default', ['styles', 'scripts'], function() {
+    gulp.watch("css/scss/*.scss", ['styles']);
+    gulp.watch("js/**/*.js", ['scripts']);
     startExpress();
 });
 
