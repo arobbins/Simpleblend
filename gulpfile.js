b85899e99770d45b32be9640bdf5b2553bc98f52
arobbins <arobbins@simpleblend.net>
2014-07-04 15:31:36 -0500

    Updated Gulp build with browser synch

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,108 @@
+/* ====================================================================
+Variables
+==================================================================== */
 var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
 	sass = require('gulp-sass'),
 	concat = require('gulp-concat'),
 	minifyCSS = require('gulp-minify-css'),
-	watch = require('gulp-watch'),
-	livereload = require('gulp-livereload'),
 	jshint = require('gulp-jshint'),
-	rename = require('gulp-rename');
-	uglify = require('gulp-uglify');
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	pngcrush = require('imagemin-pngcrush'),
+	svgo = require('imagemin-svgo'),
+	paths = {
+		html: [
+			'./**/*.html'
+		],
+		sass: [
+			'css/scss/*.scss'
+		],
+		imgs: {
+			'src' : 'imgs/*',
+			'dest' : 'imgs'
+		},
+		styles: [
+			'css/styles.min.css'
+		],
+		scripts: [
+			// Order matters for vendor libs
+			'js/vendor/angular.min.js',
+			'js/vendor/angular-ui-router.min.js',
+			'js/vendor/angular-scroll.min.js',
+			'js/vendor/ui-bootstrap-tpls-0.11.0.min.js',
+			'js/vendor/ui-utils.min.js',
 
-var paths = {
-	scripts: [
-		'js/vendor/angular.min.js',
-		'js/vendor/angular-ui-router.min.js',
-		'js/vendor/angular-scroll.min.js',
-		'js/vendor/ui-bootstrap-tpls-0.11.0.min.js',
-		'js/vendor/ui-utils.min.js',
-		'js/app/app.js',
-		'js/app/controllers/*.js',
-		'js/app/services/*.js',
-		'js/app/directives/*.js'
-	]
-};
+			'js/app/app.js',
+			'js/app/controllers/*.js',
+			'js/app/services/*.js',
+			'js/app/directives/*.js'
+		]
+	};
 
-function startExpress() {
-	var express = require('express'),
-		app = express();
+/* ====================================================================
+Local Web Server
+==================================================================== */
 
-	app.use(express.static(__dirname));
-	app.listen(9012);
-}
+gulp.task('browser-sync', function() {
+	browserSync.init(null, {
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+/* ====================================================================
+Stylesheets
+==================================================================== */
 
 gulp.task('styles', function() {
     gulp.src('css/scss/main.scss')
 		.pipe(sass({includePaths: ['scss']}))
 		.pipe(minifyCSS())
 		.pipe(rename('styles.min.css'))
-		.pipe(gulp.dest('css'));
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({
+			stream:true
+		}));
 });
 
-// Minify and concat all js scripts
+/* ====================================================================
+Scripts
+==================================================================== */
+
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
     .pipe(uglify())
-    .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('js'));
+    .pipe(concat('scripts.min.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({
+    	stream:true
+    }));
 });
 
-gulp.task('default', ['styles', 'scripts'], function() {
-    gulp.watch("css/scss/*.scss", ['styles']);
-    gulp.watch("js/**/*.js", ['scripts']);
-    startExpress();
+/* ====================================================================
+Images
+==================================================================== */
+
+gulp.task('images', function() {
+	return gulp.src(paths.imgs.src)
+	  .pipe(imagemin({
+	      progressive: true,
+	      svgoPlugins: [{removeViewBox: false}],
+	      use: [
+	      	pngcrush(),
+	      	svgo()
+	      ]
+	  }))
+	  .pipe(gulp.dest(paths.imgs.dest));
 });
 
+/* ====================================================================
+Tying everything together...
+==================================================================== */
 
+gulp.task('default', ['styles', 'scripts', 'images', 'browser-sync'], function() {
+	gulp.watch([paths.sass, paths.scripts, paths.html], ['styles', 'scripts']);
+});
