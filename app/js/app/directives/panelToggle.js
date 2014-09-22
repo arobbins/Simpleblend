cbc4fd8de9388d4e9dd3a1485e4542f82ff545da
arobbins <arobbins@simpleblend.net>
2014-09-21 22:50:39 -0500

    Removed unneeded JS from accrodian directive.

--- app/js/app/directives/panelToggle.js
+++ app/js/app/directives/panelToggle.js
@@ -8,12 +8,11 @@
 
 					$timeout(function(){
 
-						var previousTarget = null,
-							offset = 25,
+						// Time for panel to open: 0.4s.
+
+					  var offset = 25,
 							timeToScroll = 200,
 							duration = 500,
-							top  = document.documentElement.scrollTop || document.body.scrollTop,
-							panels = [],
 							activePanels = [];
 						/*
 							Function that finds the amount of pixels an element is
@@ -34,23 +33,18 @@
 
 						element.find('h2').on('click', function(){
 
-
-
-							var currentPanel = angular.element(this),
-								currentPanelTop = currentPanel[0].offsetTop - offset,
+						  var lastActivePanel,
+								top,
+								currentPanel = angular.element(this),
+								currentPanelTop = currentPanel[0].offsetTop,
 								currentPanelID = currentPanel.attr('id'),
-
 								about = document.getElementById('about'),
-								aboutThirdHeight = about.clientHeight / 2,
-
+								aboutThirdHeight = (about.clientHeight / 2),
 								work = document.getElementById('work'),
-								workTop = work.offsetTop - 30,
-
+								workTop = (work.offsetTop - 30),
 								services = document.getElementById('services'),
-								servicesThirdHeight = services.clientHeight / 2,
-
+								servicesThirdHeight = (services.clientHeight / 2),
 								nav = document.getElementsByClassName('nav');
-
 
 								// Nothing in array
 								if(activePanels.length === 0){
@@ -58,23 +52,14 @@
 
 								// Something in array
 								} else {
-
 									if(currentPanel.hasClass('panel-open')){
 										activePanels.splice(activePanels.indexOf(currentPanelID), 1);
 									} else {
 										activePanels.push(currentPanelID);
 									}
-
 								}
 
-							var lastActivePanel = document.getElementById(activePanels[activePanels.length - 1]);
-
-
-
-
-
-							// Adding the currently clicked panel to our panels array
-							panels.push(currentPanelID);
+							lastActivePanel = document.getElementById(activePanels[activePanels.length - 1]);
 
 							// If all panels are closed
 							if(scope.options.panelCounter === 0){
@@ -87,33 +72,28 @@
 									scope.options.panelCounter++;
 								});
 
-								// Scroll listener
-								$document.on('scroll', function(){
-									if(scope.options.panelCounter > 0){
-										top  = document.documentElement.scrollTop || document.body.scrollTop;
+								angular.element(nav).removeClass("panel-opened");
+								angular.element(nav).addClass("panel-closed");
 
-										$timeout(function(){
+								$timeout(function(){
+									$document.scrollToElement(currentPanel[0], 0, duration);
+
+									//Scroll listener
+									$document.on('scroll', function(){
+										if(scope.options.panelCounter > 0){
+											top = document.documentElement.scrollTop || document.body.scrollTop;
+
 											if(top <= (getOffsetTop(about) + aboutThirdHeight) || top >= (getOffsetTop(services) - servicesThirdHeight)){
 
-											angular.element(nav).addClass("panel-opened");
-											angular.element(nav).removeClass("panel-closed");
+												angular.element(nav).addClass("panel-opened");
+												angular.element(nav).removeClass("panel-closed");
 
 											} else {
 												angular.element(nav).addClass("panel-closed");
 												angular.element(nav).removeClass("panel-opened");
 											}
-										}, timeToScroll);
-
-									}
-								});
-
-								angular.element(nav).removeClass("panel-opened");
-								angular.element(nav).addClass("panel-closed");
-
-								$timeout(function(){
-									var ctop = currentPanel[0].offsetTop - offset;
-									$document.scrollTop(ctop, duration);
-
+										}
+									});
 								}, timeToScroll);
 
 							// If more than one panel is open
@@ -122,23 +102,29 @@
 								// If clicked panel is already open
 								if(angular.element(this).hasClass('panel-open')){
 
+									// Smoother scroll when more panels are open
+									if(activePanels.length > 2) {
+										// timeToScroll = 500;
+										duration = 1000;
+									}
+
 									// Decrement counter
 									scope.$apply(function(){
 										scope.options.panelCounter--;
 									});
 
+									// Toggle panel
+									angular.element(this).toggleClass('panel-open');
+									angular.element(this).find('i').toggleClass('fa-chevron-down');
+
 									// At this point, check counter again
 									if(scope.options.panelCounter >= 1){
 
 										$timeout(function(){
-											$document.scrollTop(lastActivePanel.offsetTop - offset, duration);
-										}, (timeToScroll + 300));
+											$document.scrollToElement(lastActivePanel, 0, (duration + 200));
+										}, (timeToScroll + 200));
 
 									}
-
-									// Toggle panel
-									angular.element(this).toggleClass('panel-open');
-									angular.element(this).find('i').toggleClass('fa-chevron-down');
 
 								// If we clicked an unopened panel
 								} else {
@@ -148,19 +134,19 @@
 										scope.options.panelCounter++;
 									});
 
-									// Scroll to that panel
-									$timeout(function(){
-										$document.scrollTop(currentPanelTop, duration);
-									}, timeToScroll);
-
 									// Toggle panel
 									angular.element(this).toggleClass('panel-open');
 									angular.element(this).find('i').toggleClass('fa-chevron-down');
 
+									// Scroll to that panel
+									$timeout(function(){
+										$document.scrollToElement(currentPanel[0], 0, (duration + 200));
+									}, timeToScroll);
+
 								}
 
+								// Calculate offset for hiding nav during scroll
 								var timer, refresh;
-								// Scroll listener
 								$document.on('scroll', function(){
 									refresh = function () {
 										if(scope.options.panelCounter > 1){
@@ -185,12 +171,12 @@
 												$timeout(function(){
 													angular.element(nav).addClass("panel-opened");
 													angular.element(nav).removeClass("panel-closed");
-												}, timeToScroll);
+												}, (timeToScroll + 400));
 											} else {
 												$timeout(function(){
 													angular.element(nav).addClass("panel-closed");
 													angular.element(nav).removeClass("panel-opened");
-												}, timeToScroll);
+												}, (timeToScroll + 400));
 											}
 										}
 									};
@@ -203,26 +189,12 @@
 							// Finally, check counter again. If zero, scroll to center work section
 							if(scope.options.panelCounter === 0){
 
-								scope.$apply(function(){
+								// Closer 'er up!
+								angular.element(nav).removeClass("panel-closed");
+								angular.element(nav).addClass("panel-opened");
+								$document.scrollTop(workTop, duration);
 
-									$timeout(function(){
-										$document.scrollTop(workTop, duration);
-										// Closer 'er up!
-										angular.element(nav).removeClass("panel-closed");
-										angular.element(nav).addClass("panel-opened");
-
-									}, timeToScroll);
-
-
-
-								});
-
-								// Empty the panels array
-								while(panels.length > 0) {
-								    panels.pop();
-								}
 							}
-
 						});
 					});
       		}
